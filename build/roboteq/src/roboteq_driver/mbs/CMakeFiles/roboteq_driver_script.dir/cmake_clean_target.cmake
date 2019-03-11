ccd66e436bf833da747bb2a1661cf614070e448c
Sena <sena@vt.edu>
2019-03-11 18:24:47 -0400

    this is truly the first commit

--- build/roboteq/src/roboteq_driver/mbs/CMakeFiles/roboteq_driver_script.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/turtle1/turtle_drive/devel/lib/libroboteq_driver_script.a"
-)
--- build/roboteq/src/roboteq_driver/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/turtle1/turtle_drive/src/roboteq/src/roboteq_driver/src
-# Build directory: /home/turtle1/turtle_drive/build/roboteq/src/roboteq_driver/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
