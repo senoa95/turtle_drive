9f1074977f8230a58283a56cff595c053db8fa5e
Sena <sena@vt.edu>
2019-03-11 18:24:47 -0400

    this is truly the first commit

--- build/roboteq/src/roboteq_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/turtle1/turtle_drive/src/roboteq/src/roboteq_msgs
-# Build directory: /home/turtle1/turtle_drive/build/roboteq/src/roboteq_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
