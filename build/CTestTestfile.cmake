7e6fa3115b65e7d1ed26d30a6cd4a705b2b28f5c
nishmankame <nishmankame@vt.edu>
2019-03-12 22:37:27 -0400

    Added my packeges

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,37 @@
+# CMake generated Testfile for 
+# Source directory: /home/nishanth/TurtleStuff/turtle_drive/src
+# Build directory: /home/nishanth/TurtleStuff/turtle_drive/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(driver_common/driver_common)
+subdirs(hector_slam/hector_slam)
+subdirs(hector_slam/hector_slam_launch)
+subdirs(joystick_drivers/joystick_drivers)
+subdirs(roboteq/roboteq_diagnostics)
+subdirs(hector_slam/hector_map_tools)
+subdirs(hector_slam/hector_nav_msgs)
+subdirs(roboteq/roboteq_msgs)
+subdirs(Turtles/turtle_description)
+subdirs(urg_c)
+subdirs(hector_slam/hector_geotiff)
+subdirs(hector_slam/hector_geotiff_plugins)
+subdirs(hector_slam/hector_marker_drawing)
+subdirs(roboteq/roboteq_driver)
+subdirs(driver_common/timestamp_tools)
+subdirs(driver_common/driver_base)
+subdirs(hector_slam/hector_compressed_map_transport)
+subdirs(joystick_drivers/joy)
+subdirs(laser_proc)
+subdirs(joystick_drivers/ps3joy)
+subdirs(joystick_drivers/spacenav_node)
+subdirs(hector_slam/hector_imu_attitude_to_tf)
+subdirs(hector_slam/hector_imu_tools)
+subdirs(hector_slam/hector_map_server)
+subdirs(hector_slam/hector_trajectory_server)
+subdirs(hector_slam/hector_mapping)
+subdirs(Turtles/turtle_control)
+subdirs(urg_node)
+subdirs(usb_cam)
+subdirs(joystick_drivers/wiimote)
