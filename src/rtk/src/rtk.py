9e12274831e5061170ad97a3dfd4f5e847eac6a3
sena <sena@vt.edu>
2019-10-13 16:55:57 -0400

    updated configuration directions. Created ros node to broadcast rtcm for multi-vehicle RTK operation

--- src/rtk/src/rtk.py
+++ src/rtk/src/rtk.py
@@ -4,28 +4,44 @@ import rospy
 import serial
 import socket
 from socket import error as socket_error
+from std_msgs.msg import String as type
 
-###Trimble TCP Connection Initialization
-TCP_IP = '192.168.1.201'
+def rtcm_broadcast():
+    ###Define publisher
+    pub = rospy.Publisher('rtcm', type, queue_size=10)
+    rospy.init_node('rtcm_broadcast', anonymous='True')
+    rate = rospy.Rate(50) 
 
-TCP_PORT = 5017 #this is the port number inside the trimble configuration
+    ###Trimble TCP Connection Initialization
+    TCP_IP = '192.168.1.4'
 
-BUFFER_SIZE = 4096
+    TCP_PORT = 5017 #this is the port number inside the trimble configuration
 
-# Open TCP Socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((TCP_IP, TCP_PORT))
+    BUFFER_SIZE = 4096
 
-# Open Serial connection
-ser = serial.Serial('/dev/ttyUSB0',115200,rtscts=True,dsrdtr=True)
+    # Open TCP Socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.connect((TCP_IP, TCP_PORT))
 
-#SETTING UP Novatel to take RTCM correction
-ser.write('INTERFACEMODE USB1 RTCM NOVATEL ON\r\n')
-ser.write('INSCOMMAND ENABLE\r')
-ser.write('LOG USB1 INSPVA ONTIME \r\n')
+    # Open Serial connection
+    # ser = serial.Serial('/dev/ttyUSB0',115200,rtscts=True,dsrdtr=True)
 
-while True:
-    #Recieve RTCM and write RTCM data to serial
-    RTCM = s.recv(BUFFER_SIZE)
-    ser.write(RTCM)
+    #SETTING UP Novatel to take RTCM correction
+    # ser.write('INTERFACEMODE USB1 RTCM NOVATEL ON\r\n')
+    # ser.write('INSCOMMAND ENABLE\r')
+    # ser.write('LOG USB1 INSPVA ONTIME \r\n')
+
+    while not rospy.is_shutdown():
+        #Recieve RTCM and write RTCM data to serial
+        RTCM = s.recv(BUFFER_SIZE)
+        pub.publish(RTCM)
+        rate.sleep()
+
+        # ser.write(RTCM)
+
+if __name__ == '__main__':
+    try:
+        rtcm_broadcast()
+    except rospy.ROSInterruptException:
+        pass
 
