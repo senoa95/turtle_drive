490f735bb9396b3eb6c9c9df8fcfc6bd1a1bdd5d
Sena <sena@vt.edu>
2019-10-13 17:46:53 -0400

    modified gps launcher and rtk script to use latest trimble configs

--- src/rtk/src/rtk.py
+++ src/rtk/src/rtk.py
@@ -6,7 +6,7 @@ import socket
 from socket import error as socket_error
 
 ###Trimble TCP Connection Initialization
-TCP_IP = '192.168.1.201'
+TCP_IP = '192.168.1.4'
 
 TCP_PORT = 5017 #this is the port number inside the trimble configuration
 
@@ -17,10 +17,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
 # Open Serial connection
-ser = serial.Serial('/dev/ttyUSB0',115200,rtscts=True,dsrdtr=True)
+ser = serial.Serial('/dev/ttyUSB1',115200,rtscts=True,dsrdtr=True)
 
 #SETTING UP Novatel to take RTCM correction
-ser.write('INTERFACEMODE USB1 RTCM NOVATEL ON\r\n')
+ser.write('INTERFACEMODE USB2 RTCM NOVATEL ON\r\n')
 ser.write('INSCOMMAND ENABLE\r')
 ser.write('LOG USB1 INSPVA ONTIME \r\n')
 
